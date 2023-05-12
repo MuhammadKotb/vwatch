2135fdb0d83361b6f7a87631b248b0109d2791be
MuhammadElkotb <Muhammad175ib59@gmail.com>
2023-05-12 18:36:53 +0300

    example SDL2 main loop

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 SOURCES:=src/*.cpp
 MAKEFLAGS += --no-print-directory
-MAKEFLAGS += --no-print-directory
+CFLAGS+= -std=c++20
+CFLAGS+= -D_REENTRANT -I/usr/local/include -I/usr/local/include/SDL2 
+CFLAGS+= -L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL2
 
-CFLAGS:=-std++20
 
 build: 
-	@g++ $(SOURCES) -o src/main
+	g++ $(SOURCES) -o src/main $(CFLAGS)
 	@echo BuildSucc
 
 run: build
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
+#include "SDL_events.h"
+#include "SDL_render.h"
+#include <SDL2/SDL_video.h>
 #include <iostream>
 #include <vlc/libvlc.h>
 #include <libavcodec/avcodec.h>
-
+#include <SDL2/SDL.h>
 
 int main(int argc, char* asrgv[]) {
 
 
+    SDL_Init(SDL_INIT_EVERYTHING);
 
-
-    std::cout << "Helxlo, vwatch!";
-
-
-
+    SDL_Window* window =  SDL_CreateWindow("hello", 300, 200, 800, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    bool run = true;
+    while(run) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            if(event.window.type == SDL_QUIT) {
+                run = false;
+            }
+        }
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        SDL_RenderPresent(renderer);
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
 
 }
