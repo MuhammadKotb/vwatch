b3cbb6b534f1a6468d8b285d67b635c9077026fd
MuhammadElkotb <Muhammad175ib59@gmail.com>
2023-05-13 21:54:04 +0300

    added libavcodec headers to include folder

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 SOURCES:=src/*.cpp
 MAKEFLAGS += --no-print-directory
 CFLAGS+= -std=c++20
-CFLAGS+= -D_REENTRANT -I/usr/local/include -I/usr/local/include/SDL2 -I./include/ -I./include/imgui
+CFLAGS+= -D_REENTRANT  -I./include/ 
 CFLAGS+= -L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL2
+CFLAGS+= -lavcodec
 CFLAGS+= libs/*.o
+
+
 
 
 build: 
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,29 @@
-#include <cstdlib>
 #include <iostream>
-#include <set>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_video.h>
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_opengl3.h>
 #include <imgui/imgui_impl_sdl2.h>
+extern "C" {
+    #include <SDL2/SDL.h>
+    #include <libavcodec/avcodec.h>
+}
+
 
 
 int main(int argc, char* asrgv[]) {
 
+    
     if(SDL_Init(SDL_INIT_EVERYTHING)) {
         std::cout << "SDL INIT ERROR\n";
+        SDL_Quit();
         exit(0);
     }
     SDL_Window* window =  SDL_CreateWindow("hello", 300, 200, 800, 600, SDL_WINDOW_RESIZABLE);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
     bool run = true;
+    AVCodec* decoder = avcodec_find_decoder(AV_CODEC_ID_CINEPAK);
+    AVCodecContext* codec_context = avcodec_alloc_context3(decoder);
+
+
     while(run) {
         SDL_Event event;
         while(SDL_PollEvent(&event)) {
